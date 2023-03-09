658368ab87326882dffffa22f5fed1473769f6ec
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2023-03-09 19:52:03 +0500

    fix imports 2

--- lib/Model/firebase_auth.dart
+++ lib/Model/firebase_auth.dart
@@ -1,13 +1,9 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hi_chat/Controller/sign_in_provider.dart';
 import 'package:provider/provider.dart';
-
-import '../Controller/is_signed.dart';
 
 //Google Sign in
 Future<void> signInWithGoogle(BuildContext context) async {
